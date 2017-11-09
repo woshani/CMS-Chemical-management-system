f623f7d335b152dde7be5510c38b5ae8881eaec6
DESKTOP-KHMTLEL\ummihumaira <ummihumaira1708.uh@gmail.com>
2017-11-10 00:41:34 +0800

    ni dia accept funtion n reject x jd 2 aq buat,,hahahah

--- MS/approve_student.php
+++ MS/approve_student.php
@@ -1,1 +1,59 @@
-<p>Place Your Content Here!</p>
+<p>
+
+	<div class="table-responsive">
+		<center>
+			<table id="userTable" class="table table-striped table-bordered table-hover text-center">
+				<thead>
+					<tr>
+						<th class="text-center">#</th>
+						<th class="text-center">Full Name</th>
+						<th class="text-center">Identity No</th>
+						<th class="text-center">Email</th>
+						<th class="text-center">Phone No</th>
+						<th class="text-center">Action</th>
+					</tr>
+				</thead>
+				<?php 
+				include "../connection/connection.php";
+				$user_id = $_SESSION['userid'];
+				$selectSql = "SELECT * FROM user WHERE role = 'Pending' AND supervisorid=".$user_id;
+				$selectResult = mysqli_query($conn,$selectSql);
+				if(mysqli_num_rows($selectResult) > 0)
+				{
+				$no = 1;
+					while($row = mysqli_fetch_array($selectResult))
+					{
+					?>
+					
+				<tbody>
+					<tr>
+						<td><?php echo $no; ?></td>
+						<td><?php echo $row["fname"];?> <?php echo $row["lname"];?></td>
+						<td><?php echo $row["identifyid"];?></td>
+						<td><?php echo $row["email"];?></td>
+						<td><?php echo $row["telno"];?></td>
+						<td>
+							<button type="submit" name="btnAccept" class="btn btn-success">Accept</button>
+							<button type="submit" name="btnReject" class="btn btn-success">Reject</button>
+						</td>
+					</tr>
+				<?php
+				$no++;
+					}
+				}
+				else
+				{
+				?>
+					<tr>
+						<td colspan="4">No Data</td>
+					</tr>
+				<?php
+				}
+				mysqli_close($conn);
+				?>
+				</tbody>
+			</table>
+		</center>	
+	</div>
+   
+</p>
