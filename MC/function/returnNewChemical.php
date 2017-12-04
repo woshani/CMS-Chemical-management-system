a237cd7ebb5f3ed59ce7e37dbe170aadad8dcc6b
DESKTOP-KHMTLEL\ummihumaira <ummihumaira1708.uh@gmail.com>
2017-12-05 00:21:33 +0800

    with email

--- MC/function/returnNewChemical.php
+++ MC/function/returnNewChemical.php
@@ -3,12 +3,34 @@ include "../../connection/connection.php";
 
 require_once("../../plugins/phpmailer/class.phpmailer.php");
 	
+	$email=$_POST['email'];
+	$message=$_POST['message'];
+	$subject = $_POST['sub'];
 	
 	$status = $_POST['status'];
 	$chemicaInlId = $_POST['chemicalId'];
 	$chemicalUserId = $_POST['cserId'];
 	$chemicalpeminjam = $_POST['peminjam'];
 	
+	$mailer = new PHPMailer();
+	$mailer->IsSMTP();
+	$mailer->Host = 'ssl://smtp.gmail.com:465';
+	$mailer->SMTPAuth = TRUE;
+	$mailer->Username = 'cmsutem@gmail.com';  // Change this to your gmail adress
+	$mailer->Password = 'cmsUTeM1234';  // Change this to your gmail password
+	$mailer->From = 'cmsutem@gmail.com';  // This HAVE TO be your gmail adress
+	$mailer->FromName = 'CMSUTeM'; // This is the from name in the email, you can put anything you like here
+	$mailer->Subject = $subject;
+	$mailer->Body = $message;
+	$mailer->AddAddress($email); 
+
+	if(!$mailer->Send())
+	{
+	echo "fail";
+
+	}
+	else
+	{
 
 		$query = "UPDATE chemicalusage SET status = '".$status."' ,enddate=now() where ciid = '".$chemicaInlId."' AND userid = '".$chemicalpeminjam."';";
 		$query .= "UPDATE chemicalIn SET status = '".$status."' where userid = ".$chemicalUserId." AND ciid = ".$chemicaInlId.";";
@@ -18,6 +40,6 @@ require_once("../../plugins/phpmailer/class.phpmailer.php");
 				}else{
 					echo $query;
 				}
-	
+	}
 mysqli_close($conn);
 ?>
--- MC/function/reuseNewChemical.php
+++ MC/function/reuseNewChemical.php
@@ -1,9 +1,34 @@
 <?php 
 include "../../connection/connection.php";
 	
+require_once("../../plugins/phpmailer/class.phpmailer.php");
+
+	$email=$_POST['email'];
+	$message=$_POST['message'];
+	$subject = $_POST['sub'];
+	
 	$chemicaInlId = $_POST['chemicalId'];
 	$chemicalUserId = $_POST['cserId'];
 	
+	$mailer = new PHPMailer();
+	$mailer->IsSMTP();
+	$mailer->Host = 'ssl://smtp.gmail.com:465';
+	$mailer->SMTPAuth = TRUE;
+	$mailer->Username = 'cmsutem@gmail.com';  // Change this to your gmail adress
+	$mailer->Password = 'cmsUTeM1234';  // Change this to your gmail password
+	$mailer->From = 'cmsutem@gmail.com';  // This HAVE TO be your gmail adress
+	$mailer->FromName = 'CMSUTeM'; // This is the from name in the email, you can put anything you like here
+	$mailer->Subject = $subject;
+	$mailer->Body = $message;
+	$mailer->AddAddress($email); 
+
+	if(!$mailer->Send())
+	{
+	echo "fail";
+
+	}
+	else
+	{
 	
 		$query = "UPDATE chemicalin set status = 'In Use' WHERE ciid = '".$chemicaInlId."';";
 		$query .= "INSERT INTO 
@@ -15,5 +40,6 @@ include "../../connection/connection.php";
 				}else{
 					echo $query;
 				}
+	}
 mysqli_close($conn);
 ?>
